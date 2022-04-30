bb21686269912a0b67604ddbef877bf83cddc68e
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-05-01 03:16:01 +0900

    repository for estimate domain

--- src/main/java/com/muah/muahbackend/domain/estimate/repository/SheetRepository.java
+++ src/main/java/com/muah/muahbackend/domain/estimate/repository/SheetRepository.java
@@ -3,5 +3,5 @@ package com.muah.muahbackend.domain.estimate.repository;
 import com.muah.muahbackend.domain.estimate.entity.Sheet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public abstract class SheetRepository implements JpaRepository<Sheet, Long> {
+public interface SheetRepository extends JpaRepository<Sheet, Long> {
 }
