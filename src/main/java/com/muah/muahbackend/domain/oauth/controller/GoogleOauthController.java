b2f556405e15262e1290b82417ae6d40385a371d
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-05-05 09:06:53 +0900

    google oauth - get user info from google api server

--- src/main/java/com/muah/muahbackend/domain/oauth/controller/GoogleOauthController.java
+++ src/main/java/com/muah/muahbackend/domain/oauth/controller/GoogleOauthController.java
@@ -1,12 +1,11 @@
 package com.muah.muahbackend.domain.oauth.controller;
 
+import com.muah.muahbackend.domain.oauth.dto.GoogleLoginDto;
 import com.muah.muahbackend.domain.oauth.service.GoogleOauthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -23,4 +22,15 @@ public class GoogleOauthController {
         oauthService.request();
     }
 
+    /**
+     * Social Login API Server 요청에 의한 callback 을 처리
+     * @param code API Server 로부터 넘어노는 code
+     * @return SNS Login 요청 결과로 받은 Json 형태의 String 문자열 (access_token, refresh_token 등)
+     */
+    @GetMapping(value = "google/callback")
+    public ResponseEntity<GoogleLoginDto> callback(
+            @RequestParam(name = "code") String code) {
+        log.info(">> 소셜 로그인 API 서버로부터 받은 code :: {}", code);
+        return oauthService.requestAccessToken(code);
+    }
 }
--- src/main/java/com/muah/muahbackend/domain/oauth/service/GoogleOauthService.java
+++ src/main/java/com/muah/muahbackend/domain/oauth/service/GoogleOauthService.java
@@ -1,8 +1,18 @@
 package com.muah.muahbackend.domain.oauth.service;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.muah.muahbackend.domain.oauth.dto.GoogleLoginDto;
+import com.muah.muahbackend.domain.oauth.dto.GoogleLoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -22,10 +32,14 @@ public class GoogleOauthService {
     private String GOOGLE_CALLBACK_URL;
     @Value("${google.client.secret}")
     private String GOOGLE_CLIENT_SECRET;
+    @Value("${google.token.url}")
+    private String GOOGLE_TOKEN_BASE_URL;
+    @Value("${google.auth.url}")
+    private String GOOGLE_AUTH_URL;
 
     public void request() {
         Map<String, Object> params = new HashMap<>();
-        params.put("scope", "email");
+        params.put("scope", "email%20profile%20openid");
         params.put("response_type", "code");
         params.put("client_id", GOOGLE_CLIENT_ID);
         params.put("redirect_uri", GOOGLE_CALLBACK_URL);
@@ -41,5 +55,41 @@ public class GoogleOauthService {
         }catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public ResponseEntity<GoogleLoginDto> requestAccessToken(String code) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("code", code);
+        params.put("client_id", GOOGLE_CLIENT_ID);
+        params.put("client_secret", GOOGLE_CLIENT_SECRET);
+        params.put("redirect_uri", GOOGLE_CALLBACK_URL);
+        params.put("grant_type", "authorization_code");
+        try{
+            ResponseEntity<String> responseEntity =
+                    restTemplate.postForEntity(GOOGLE_TOKEN_BASE_URL, params, String.class);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            GoogleLoginResponse googleLoginResponse = objectMapper.readValue(responseEntity.getBody(), new TypeReference<GoogleLoginResponse>() {});
+
+            String jwtToken = googleLoginResponse.getIdToken();
+
+            String requestUrl = UriComponentsBuilder.fromHttpUrl(GOOGLE_AUTH_URL+"/tokeninfo").queryParam("id_token",jwtToken).toUriString();
+            String resultJson = restTemplate.getForObject(requestUrl, String.class);
+
+            if (resultJson != null) {
+                GoogleLoginDto userinfoDto = objectMapper.readValue(resultJson, new TypeReference<GoogleLoginDto>() {});
+                return ResponseEntity.ok().body(userinfoDto);
+            }
+            else {
+                throw new Exception("Google OAuth failed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.badRequest().body(null);
     }
 }
