e8d94feac421591f20dc27f711e9c873601f5d12
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-05-12 21:24:30 +0900

    fix data.sql error

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,3 @@
 INSERT INTO USERS(USER_ID, USER_EMAIL, PASSWORD, USER_ROLE)
-<<<<<<< HEAD
 VALUES(1, 'admin@example.com', '1234', 'ROLE_USER');
-=======
-VALUES(1, 'admin@example.com', '1234', 'ROLE_USER');
->>>>>>> e96121aa5ade70d1968e2f489f67976dacba22f1
+
