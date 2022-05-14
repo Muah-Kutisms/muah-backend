daf3ea61832d6280fe53d978e1eafb1ddb2b93cb
dahyunkim <dahyun_ee@naver.com>
2022-05-15 02:27:43 +0900

    travis.ci test final

---
+++ src/main/java/com/muah/muahbackend/domain/estimate/controller/SheetController.java
@@ -0,0 +1,25 @@
+package com.muah.muahbackend.domain.estimate.controller;
+
+
+import com.muah.muahbackend.domain.user.dto.TokenDto;
+import com.muah.muahbackend.domain.user.dto.TokenRequestDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/funeral/estimate")
+@RequiredArgsConstructor
+@Api(tags = "견적서 생성 API")
+public class SheetController {
+
+    @ApiOperation(value = "견적서 조회")
+    @GetMapping("/")
+    public void getSheet(@RequestBody TokenRequestDto tokenRequestDto) {
+
+
+    }
+
+}
